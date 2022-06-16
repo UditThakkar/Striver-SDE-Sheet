9761a54ad5ed1325434d9d8ee050a1dc9c93bd16
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2022-06-17 01:53:56 +0530

    Added efficient solution

--- Day 6/Count Subarrays with Given XOR.cpp
+++ Day 6/Count Subarrays with Given XOR.cpp
@@ -15,3 +15,22 @@ int subarraysXor(vector<int> &arr, int x)
     }
     return count;
 }
+
+// Efficient approach using map
+#include <bits/stdc++.h> 
+int subarraysXor(vector<int> &arr, int x)
+{
+    //    Write your code here.
+    unordered_map<int,int> mp;
+    int cnt = 0,xorr=0,n=arr.size();
+    for(int i=0;i<n;i++){
+        xorr ^= arr[i];
+        if(mp.find(xorr^x)!=mp.end()){
+            cnt+=mp[xorr^x];
+        }
+        if(xorr==x) cnt++;
+        if(mp.find(xorr)!=mp.end()) mp[xorr]++;
+        else mp[xorr] = 1;
+    }
+    return cnt;
+}
