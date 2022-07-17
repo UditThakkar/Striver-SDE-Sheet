2049f4b53c618709657cbbeb0c1936edd11aea4c
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2022-07-18 01:51:05 +0530

    Create NextSmaller.cpp

---
+++ Day 28/NextSmaller.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    // Write your code here.
+    vector<int> ans(n);
+    stack<int> s;
+    for (int i = n - 1; i >= 0; i--) {
+        while (!s.empty() && s.top() >= arr[i]) {
+            s.pop();
+        }
+
+        if (s.empty()) {
+            ans[i] = -1;
+        } 
+        else {
+            ans[i] = s.top();
+        }
+
+        s.push(arr[i]);
+    }
+
+    return ans;
+}
