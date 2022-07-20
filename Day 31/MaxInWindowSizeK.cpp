a831198afeef9c7119e8583f7ba2508f22e647c1
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2022-07-21 01:39:27 +0530

    Update MaxInWindowSizeK.cpp

--- Day 31/MaxInWindowSizeK.cpp
+++ Day 31/MaxInWindowSizeK.cpp
@@ -22,3 +22,23 @@ vector<int> slidingWindowMaximum(vector<int> &nums, int &k)
     }
     return ans;
 }
+
+
+// efficient 
+#include<bits/stdc++.h>
+vector<int> slidingWindowMaximum(vector<int> &nums, int &k)
+{
+    //    Write your code here.
+    deque<int> dq;
+    vector<int> ans;
+    int n = nums.size();
+    for(int i=0;i<n;i++){
+        if(!dq.empty() and dq.front()==i-k) dq.pop_front();
+           while (!dq.empty() && nums[dq.back()] < nums[i])
+          dq.pop_back();
+
+        dq.push_back(i);
+        if (i >= k - 1) ans.push_back(nums[dq.front()]);
+      }
+      return ans;
+}
