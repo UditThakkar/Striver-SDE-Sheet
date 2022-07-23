afadeded792698b05119c05167217a73326e883d
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2022-07-24 01:13:13 +0530

    Added efficient approach

--- Day 34/KthSmallestInBST.cpp
+++ Day 34/KthSmallestInBST.cpp
@@ -32,3 +32,27 @@ int kthSmallest(TreeNode<int> *root, int k)
     fun(root,trav);
     return trav[k-1];
 }
+
+
+// efficient
+
+TreeNode<int> *fun(TreeNode<int> *root, int &k){
+    if(root==NULL)
+    return NULL;
+    
+    TreeNode<int> *left=fun(root->left,k);
+    if(left!=NULL)
+    return left;
+    k--;
+    if(k==0)
+    return root;
+    
+    return fun(root->right,k);
+}
+
+int kthSmallest(TreeNode<int> *root, int k)
+{
+	//	Write the code here.
+    TreeNode<int> *curr = fun(root,k);
+    return curr->data;
+}
